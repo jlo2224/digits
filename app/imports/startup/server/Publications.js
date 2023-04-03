1d566ad47185560e35f534285b80ed0b643cbba6
jlo2224 <jaredlo@hawaii.edu>
2023-04-02 22:06:46 -1000

    First attempt for notes
    Almost got it to work perfectly. Got the user interface working but couldn't get the submit to work

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
 import { Contacts } from '../../api/contact/Contact.js';
+import { Notes } from '../../api/note/Notes.js';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise publish nothing.
@@ -23,6 +24,14 @@ Meteor.publish(Stuffs.adminPublicationName, function () {
 });
 
 Meteor.publish(Contacts.userPublicationName, function () {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return Contacts.collection.find({ owner: username });
+  }
+  return this.ready();
+});
+
+Meteor.publish(Notes.userPublicationName, function () {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
     return Contacts.collection.find({ owner: username });
--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -5,17 +5,22 @@ import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
 import Contact from '../components/Contact';
 import { Contacts } from '../../api/contact/Contact';
+import { Notes } from '../../api/note/Notes';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const ListContacts = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { ready, contacts } = useTracker(() => {
+  const { ready, contacts, notes } = useTracker(() => {
     const sub = Meteor.subscribe(Contacts.userPublicationName);
+    const subNotes = Meteor.subscribe(Notes.userPublicationName);
     const rdy = sub.ready();
+    const rdyNotes = subNotes.ready();
     const contactItem = Contacts.collection.find({}).fetch();
+    const notesItem = Notes.collection.find({}).fetch();
     return {
-      ready: rdy,
+      ready: rdy && rdyNotes,
       contacts: contactItem,
+      notes: notesItem,
     };
   });
 
@@ -27,7 +32,13 @@ const ListContacts = () => {
             <h2>List Contacts</h2>
           </Col>
           <Row xs={1} md={2} lg={3} className="g-4">
-            {contacts.map((contact) => (<Col key={contact._id}><Contact contact={contact} /></Col>))}
+            {contacts.map((contact) => (
+              <Col key={contact._id}><Contact
+                contact={contact}
+                notes={notes.filter(note => (note.contactId === contact._id))}
+              />
+              </Col>
+            ))}
           </Row>
         </Col>
       </Row>
